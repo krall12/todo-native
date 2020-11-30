e7e5de66ef1d3d3ec5d77c3c966e4181baf242ea
Benjamin Krall <benjaminkrall@gmail.com>
2020-11-30 15:59:21 -0500

    add plus button

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,14 +1,28 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
+import { Button, TouchableOpacity } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 
 import TodoScreen from './Todos'
 
 const HomeStack = createStackNavigator()
 
 export default function HomeScreen() {
+  const openCreateSheet = () => console.log('hi')
+
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="Todos" component={TodoScreen} />
+      <HomeStack.Screen
+        name="Todos"
+        component={TodoScreen}
+        options={{
+          headerRight: () => (
+            <TouchableOpacity onPress={openCreateSheet} style={{ marginRight: 20 }}>
+              <Ionicons name="ios-add" size={34} color="blue" />
+            </TouchableOpacity>
+          ),
+        }}
+      />
     </HomeStack.Navigator>
   )
 }
