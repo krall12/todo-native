3a91d41786348bb93cb27876f9fa9c9c27f48184
Benjamin Krall <benjaminkrall@gmail.com>
2020-12-02 16:35:14 -0500

    refactor auth context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Text } from 'react-native'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export const AuthContext = React.createContext<any>({})
 
 export default function AuthProvider({ children }: any) {
-  const [state, dispatch] = React.useReducer(
+  const [state, setState] = useState({
+    isSignedIn: false,
+    userToken: null,
+    user: null,
+  })
+
+  const [oldState, dispatch] = React.useReducer(
     (prevState: any, action: any) => {
       switch (action.type) {
         case 'RESTORE_TOKEN':
@@ -41,68 +47,58 @@ export default function AuthProvider({ children }: any) {
     }
   )
 
-  React.useEffect(() => {
-    // Fetch the token from storage then navigate to our appropriate place
-    const bootstrapAsync = async () => {
-      let userToken
-      let user
+  const restoreToken = (token: string, user: any) => {
+    setState({ isSignedIn: true, userToken: token, user })
+  }
 
+  const signIn = async (data: { email: string; password: string }) => {
+    const result = await fetch('http://192.168.1.172:3001/api/login', {
+      method: 'POST',
+      body: JSON.stringify({ email: data.email, password: data.password }),
+      headers: { 'Content-Type': 'application/json' },
+    }).then((res) => res.json())
+
+    if (result.token) {
+      AsyncStorage.setItem('userToken', result.token)
+      AsyncStorage.setItem('user', JSON.stringify(result.user))
+      setState({ isSignedIn: true, userToken: result.token, user: result.user })
+    }
+  }
+
+  const signOut = () => {
+    AsyncStorage.removeItem('userToken')
+    AsyncStorage.removeItem('user')
+    setState({ isSignedIn: false, userToken: null, user: null })
+  }
+
+  const authContext = {
+    user: state.user,
+    userToken: state.userToken,
+    signIn,
+    signOut,
+  }
+
+  useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    ;(async () => {
       try {
-        userToken = await AsyncStorage.getItem('userToken')
-        user = await AsyncStorage.getItem('user')
+        const userToken = await AsyncStorage.getItem('userToken')
+        const user = await AsyncStorage.getItem('user')
 
         // This will switch to the App screen or Auth screen and this loading
         // screen will be unmounted and thrown away.
-        if (userToken && user) {
-          dispatch({
-            type: 'RESTORE_TOKEN',
-            token: userToken,
-            user: JSON.parse(user),
-          })
-        }
+        restoreToken(userToken, user)
       } catch (e) {
         // Restoring token failed
-        dispatch({ type: 'SIGN_OUT' })
+        signOut()
       }
-    }
-
-    bootstrapAsync()
+    })()
   }, [])
-
-  const authContext = React.useMemo(
-    () => ({
-      user: state.user,
-      userToken: state.userToken,
-      signIn: async (data: any) => {
-        // In a production app, we need to send some data (usually username, password) to server and get a token
-        // We will also need to handle errors if sign in failed
-        // After getting token, we need to persist the token using `AsyncStorage`
-        // In the example, we'll use a dummy token
-        try {
-          const result = await fetch('http://192.168.1.172:3001/api/login', {
-            method: 'POST',
-            body: JSON.stringify({ email: data.email, password: data.password }),
-            headers: { 'Content-Type': 'application/json' },
-          })
-
-          const res = await result.json()
-          console.log(res)
-          if (res.token) {
-            dispatch({ type: 'SIGN_IN', token: res.token, user: res.user })
-          }
-        } catch (error) {
-          console.log(error)
-        }
-      },
-      signOut: () => dispatch({ type: 'SIGN_OUT' }),
-    }),
-    [state]
-  )
 
   return (
     <AuthContext.Provider value={authContext}>
       {children}
-      <Text>{JSON.stringify(authContext.user, null, 2)}</Text>
+      <Text>{JSON.stringify(state, null, 2)}</Text>
     </AuthContext.Provider>
   )
 }
